b52b48dc0106923d07a11137af9fd6d7611b540d
cristian <cristian.borraez@axity.com>
2020-10-23 09:23:10 -0500

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 import requests
+
 app = Flask(__name__)
 
 url = "https://ipinfo.io/"
+url2 = "https://librespeed.org/"
 
 @app.route('/ip')
 def hello_world():
-    response = requests.get(url)
+    endpoint = "/json"
+    client_ip = request.environ.get('HTTP_X_FORWARDED_FOR')
+    print(client_ip)
+    response = requests.get(url+client_ip+endpoint)
     print(response.text)
     return response.text
 
