3bcb39c615b6b82b3febf276b330732f89dc462a
cristian <cristian.borraez@axity.com>
2020-10-23 09:56:43 -0500

    uio

--- application.py
+++ application.py
@@ -1,26 +1,28 @@
 import socket
-
-from flask import Flask, request
+from flask_socketio import SocketIO
+from flask import Flask, request, render_template
 import requests
 
+async_mode = None
 app = Flask(__name__)
+socket_ = SocketIO(app, async_mode=async_mode)
 
 url = "https://ipinfo.io/"
 url2 = "https://librespeed.org/"
 
 @app.route('/ip')
 def hello_world():
-    #endpoint = "/json"
-    #client_ip = request.environ.get('HTTP_X_FORWARDED_FOR')
-    #print(client_ip)
-    #response = requests.get(url+client_ip+endpoint)
-    #print(response.text)
-    otro()
-    return "ya"#response.text
+    endpoint = "/json"
+    client_ip = request.environ.get('HTTP_X_FORWARDED_FOR')
+    print(client_ip)
+    response = requests.get(url+client_ip+endpoint)
+    print(response.text)
+    #otro()
+    return "ya"+" "+response.text
 
 def otro():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('0.0.0.0', 8000))
+    s.bind(('0.0.0.0', 5500))
     s.listen(1)
     # Estas son las variables del cliente
     conn, addr = s.accept()
@@ -28,4 +30,5 @@ def otro():
     s.close()
 
 if __name__ == '__main__':
+    #socket_.run(app)
     app.run(host='0.0.0.0')
