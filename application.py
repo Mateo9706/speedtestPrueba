d516925dba72a50647e7046d5f6f43e33665e72a
cristian <cristian.borraez@axity.com>
2020-10-25 18:25:55 -0500

    Update application.py

--- application.py
+++ application.py
@@ -27,10 +27,10 @@ def hello_world():
     #convert = client_ip.split(":")
     #print(convert)
     print(client_ip)
-    #ifd = get_ip()
-    #result = gt(ifd)
+    ifd = get_ip()
+    result = gt(ifd)
 
-    #print(ifd)
+    print(ifd)
     #response = requests.get(url+client_ip+endpoint)
     #print(response.text)
 
@@ -45,7 +45,7 @@ def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         # doesn't even have to be reachable
-        s.connect(('192.255.255.255', 1))
+        s.connect(('8.8.8.8', 1))
         IP = s.getsockname()[0]
         print(s.getsockname())
     except Exception:
